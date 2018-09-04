882b6dae9c83ca6c02a8b9704467e4b3a8c4b962
Eliano <setekpo@hotmail.fr>
2018-09-04 14:01:37 +0000

    add recretement

---
+++ Btp/Models/Recrutement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Btp.Models
+{
+    public class Recrutement
+    {
+        public int Id { get; set; }
+        public String Post { get; set; }
+        public DateTime DatePublication { get; set; }
+        public String Niveau { get; set; }
+
+
+
+    }
+}
